6fa187bb2090c1dc2d64fc0d2bc1fd221f7d02f2
christi-kinsta <93399634+christi-kinsta@users.noreply.github.com>
2023-09-12 15:44:08 -0600

    Update main.go to show Go version

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"os"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
+    "fmt"
+    "runtime"
 )
 
-func getEnv(key, fallback string) string {
-    if value, ok := os.LookupEnv(key); ok {
-        return value
-    }
-    return fallback
-}
-
 func main() {
-	engine := html.New("./views", ".html")
-
-	app := fiber.New(fiber.Config{
-        Views: engine,
-    })
-
-    app.Get("/", func(c *fiber.Ctx) error {
-        // Render index template
-        return c.Render("index", nil)
-    })
-	
-	app.Listen(":"+getEnv("PORT", "8080"))
+    fmt.Println("Hello World!")
+    fmt.Printf("The application was built with the Go version: %s\n", runtime.Version())
 }
