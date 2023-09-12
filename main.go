ef3de349fe096a5e8c64cb16b9efbc7a3430e989
christi-kinsta <93399634+christi-kinsta@users.noreply.github.com>
2023-09-12 15:56:21 -0600

    Revert main.go

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "runtime"
+	"os"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
+func getEnv(key, fallback string) string {
+    if value, ok := os.LookupEnv(key); ok {
+        return value
+    }
+    return fallback
+}
+
 func main() {
-    fmt.Println("Hello World!")
-    fmt.Printf("The application was built with the Go version: %s\n", runtime.Version())
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+        Views: engine,
+    })
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        // Render index template
+        return c.Render("index", nil)
+    })
+	
+	app.Listen(":"+getEnv("PORT", "8080"))
 }
